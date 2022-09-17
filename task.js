0e0fa67f1562681f9ec857402c828777db730e17
Eugene Tregubov <johnnydeadman0@gmail.com>
2022-09-17 18:43:28 +0300

    code in js is done

--- task.js
+++ task.js
@@ -1,6 +1,19 @@
+"use strict";
+
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
+  let arr = [];
+  if (a===0) {
+    return "a cannot be equal to zero. Please try again";
+  }
+  let d = Math.pow(b,2) - 4*a*c;
+  if (d>0) {
+    arr[0] = ((-b+Math.sqrt(d)) / (2*a));
+    arr[1] = ((-b-Math.sqrt(d)) / (2*a));
+    return arr;
+  } else if (d===0) {
+    arr[0]=-b/(2*a);
+    return arr;
+  } else if (d<0)
   return arr; // array
 }
 
